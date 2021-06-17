d0b547ed914a8e2f2bb75f58be17783db62bbc3b
mpaweria <ms.paweria@gmail.com>
2021-06-17 23:09:11 +0530

    Made about page

--- Photo Gallery/script.js
+++ Photo Gallery/script.js
@@ -1,5 +1,6 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+    header.classList.toggle("sticky1", window.scrollY > 0 && window.scrollY < 615);
+    header.classList.toggle("sticky2", window.scrollY > 615 && window.scrollY < 1230);
 });
 
