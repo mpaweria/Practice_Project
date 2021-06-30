676ee5c15b102b2a616a2585680f115b8071376c
mpaweria <ms.paweria@gmail.com>
2021-06-30 22:35:44 +0530

    Flexbox quote cards

--- Photo Gallery/script.js
+++ Photo Gallery/script.js
@@ -1,5 +1,5 @@
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky1", window.scrollY > 0 && window.scrollY < 615);
-    header.classList.toggle("sticky2", window.scrollY > 615 && window.scrollY < 1230);
-});
+// window.addEventListener("scroll", function(){
+//     var header = document.querySelector("header");
+//     header.classList.toggle("sticky1", window.scrollY > 0 && window.scrollY < 615);
+//     header.classList.toggle("sticky2", window.scrollY > 615 && window.scrollY < 1230);
+// });
